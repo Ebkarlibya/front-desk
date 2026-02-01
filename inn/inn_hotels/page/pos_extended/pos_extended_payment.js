87118e9e6c1c1407b8a072384cd2db97f368e871
SofianSaleh <sofiane.saleh11@gmail.com>
2026-02-01 11:47:29 +0200

    [Fix]: Point of sell default mop

--- inn/inn_hotels/page/pos_extended/pos_extended_payment.js
+++ inn/inn_hotels/page/pos_extended/pos_extended_payment.js
@@ -29,12 +29,16 @@ frappe.require(["point-of-sale.bundle.js", "inn-pos.bundle.js"], function () {
         render_payment_section() {
             const doc = this.events.get_frm().doc;
             const payments = doc.payments;
+            console.log(payments, 'paymentsasdfasdfasdf')
+            console.log(doc, 'docasdfasdfasdf')
             for (const p of payments) {
+                if (p.default) {
+                    p.amount = flt(doc.total);
+                }
                 frappe.model
-                    .set_value(p.doctype, p.name, 'amount', 0)
+                    .set_value(p.doctype, p.name, 'amount', p.amount)
                     .then(() => this.update_totals_section())
             }
-
 
             this.render_payment_mode_dom();
             this.make_invoice_fields_control();
